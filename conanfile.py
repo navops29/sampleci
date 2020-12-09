6532d681b3f9b3e2d04721288cf5944b69f6ada5
navops29 <snavneethchandar@gmail.com>
2020-12-09 20:04:37 +0530

    cleanup unused code

--- conanfile.py
+++ conanfile.py
@@ -16,18 +16,6 @@ class MathlibConan(ConanFile):
         cmake.configure(source_dir="mathlib")
         cmake.build()
         cmake.install()
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
-
-    """def package(self):
-        self.copy("*.h", dst="include", src="mathlib")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)"""
 
     def package_info(self):
         self.cpp_info.libs = ["mathlib"]
